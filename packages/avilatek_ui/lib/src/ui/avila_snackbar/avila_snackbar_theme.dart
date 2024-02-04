131488eeabd5dfd0a29cc2608c44b7dc201b96e6
Carlos Doffiny S-V <carlosdoffinysv@gmail.com>
2024-02-04 13:36:30 -0400

    chore: 📝 Added AvilaSnackBarTheme documentation

--- packages/avilatek_ui/lib/src/ui/avila_snackbar/avila_snackbar_theme.dart
+++ packages/avilatek_ui/lib/src/ui/avila_snackbar/avila_snackbar_theme.dart
@@ -1,6 +1,12 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:flutter/material.dart';
 
+/// `AvilaSnackBarTheme` class is being used to handle the theme of `AvilaSnackBar`
+/// for `default`, `success`, `failure`, `info` and `warning` snackbar types
+
 class AvilaSnackBarTheme extends ThemeExtension<AvilaSnackBarTheme> {
+  /// `AvilaSnackBarTheme` constructor
   const AvilaSnackBarTheme({
     this.defaultThemeData,
     this.successThemeData,
@@ -9,12 +15,34 @@ class AvilaSnackBarTheme extends ThemeExtension<AvilaSnackBarTheme> {
     this.warningThemeData,
   });
 
+  /// [defaultThemeData] is used to handle the default theme of `AvilaSnackBar`
   final SnackBarThemeData? defaultThemeData;
+
+  /// [successThemeData] is used to handle the success theme of `AvilaSnackBar`
   final SnackBarThemeData? successThemeData;
+
+  /// [failureThemeData] is used to handle the failure theme of `AvilaSnackBar`
   final SnackBarThemeData? failureThemeData;
+
+  /// [infoThemeData] is used to handle the info theme of `AvilaSnackBar`
   final SnackBarThemeData? infoThemeData;
+
+  /// [warningThemeData] is used to handle the warning theme of `AvilaSnackBar`
   final SnackBarThemeData? warningThemeData;
 
+  /// `copyWith` is used to copy the current `AvilaSnackBarTheme` and update the provided values
+  /// and return a new `AvilaSnackBarTheme`
+  ///
+  /// [defaultThemeData] is used to update the default theme of `AvilaSnackBar`
+  ///
+  /// [successThemeData] is used to update the success theme of `AvilaSnackBar`
+  ///
+  /// [failureThemeData] is used to update the failure theme of `AvilaSnackBar`
+  ///
+  /// [infoThemeData] is used to update the info theme of `AvilaSnackBar`
+  ///
+  /// [warningThemeData] is used to update the warning theme of `AvilaSnackBar`
+  ///
   @override
   ThemeExtension<AvilaSnackBarTheme> copyWith({
     SnackBarThemeData? defaultThemeData,
@@ -31,6 +59,21 @@ class AvilaSnackBarTheme extends ThemeExtension<AvilaSnackBarTheme> {
         warningThemeData: warningThemeData ?? this.warningThemeData,
       );
 
+  /// `lerp` is used to linearly interpolate between two `AvilaSnackBarTheme`
+  /// and return a new `AvilaSnackBarTheme`
+  ///
+  /// [other] is used to provide the other `AvilaSnackBarTheme` to interpolate with
+  ///
+  /// [t] is used to provide the value to interpolate
+  ///
+  /// returns a new `AvilaSnackBarTheme` after interpolation
+  ///
+  /// if [other] is not `AvilaSnackBarTheme` then it will return the current `AvilaSnackBarTheme`
+  /// without any interpolation
+  ///
+  /// if [other] is `AvilaSnackBarTheme` then it will interpolate the `defaultThemeData`, `successThemeData`,
+  /// `failureThemeData`, `infoThemeData` and `warningThemeData` and return a new `AvilaSnackBarTheme`
+  ///
   @override
   ThemeExtension<AvilaSnackBarTheme> lerp(
     covariant ThemeExtension<AvilaSnackBarTheme>? other,
