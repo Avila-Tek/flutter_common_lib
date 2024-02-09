e448a9cd6ec6574aa7a7b0d2e41e371400dafd0d
Erick Ortega <erickortegadn@gmail.com>
2024-02-09 15:03:39 -0400

    feat: :sparkles: Avila Custom Theme

---
+++ packages/avilatek_ui/lib/src/ui/avila_custom_theme.dart
@@ -0,0 +1,35 @@
+import 'package:avilatek_ui/avilatek_ui.dart';
+import 'package:flutter/material.dart';
+
+/// [AvilaCustomTheme] class is being used to handle the theme of
+/// `AvilaCustomTheme`. Here, we can handle the theme of `TitleWrapperStyle`,
+/// `SelectorButtonStyle` and `AvilaSnackBarTheme`
+class AvilaCustomTheme {
+  /// [AvilaCustomTheme] class is being used to handle the theme of
+  /// AvilaTek widgets
+  const AvilaCustomTheme({
+    required this.titleWrapperStyle,
+    required this.selectorButtonStyle,
+    required this.avilaSnackBarTheme,
+  });
+
+  /// `titleWrapperStyle` is being used to handle the theme of
+  /// [TitleWrapper] widget
+  final TitleWrapperStyle titleWrapperStyle;
+
+  /// `selectorButtonStyle` is a is being used to handle the theme of
+  /// [SelectorButton] widget
+  final SelectorButtonStyle selectorButtonStyle;
+
+  /// `avilaSnackBarTheme` is being used to handle the theme of
+  /// [AvilaSnackBar] widget
+  final AvilaSnackBarTheme avilaSnackBarTheme;
+
+  /// `extensions` is used to get all AvilaTek theme extensions, to then
+  /// define in the `MaterialApp` theme
+  Iterable<ThemeExtension<dynamic>> get extensions => {
+        titleWrapperStyle,
+        selectorButtonStyle,
+        avilaSnackBarTheme,
+      };
+}
