cba799ba2eefc82aabdcb12b1267b55cbeb6b3d9
Erick Ortega <erickortegadn@gmail.com>
2024-02-20 10:53:59 -0400

    refactor: :recycle: PR fixes

--- packages/avilatek_ui/lib/src/ui/avila_custom_theme.dart
+++ packages/avilatek_ui/lib/src/ui/avila_custom_theme.dart
@@ -8,28 +8,28 @@ class AvilaCustomTheme {
   /// [AvilaCustomTheme] class is being used to handle the theme of
   /// AvilaTek widgets
   const AvilaCustomTheme({
-    required this.titleWrapperStyle,
-    required this.selectorButtonStyle,
-    required this.avilaSnackBarTheme,
+    this.titleWrapperStyle,
+    this.selectorButtonStyle,
+    this.avilaSnackBarTheme,
   });
 
   /// `titleWrapperStyle` is being used to handle the theme of
   /// [TitleWrapper] widget
-  final TitleWrapperStyle titleWrapperStyle;
+  final TitleWrapperStyle? titleWrapperStyle;
 
   /// `selectorButtonStyle` is a is being used to handle the theme of
   /// [SelectorButton] widget
-  final SelectorButtonStyle selectorButtonStyle;
+  final SelectorButtonStyle? selectorButtonStyle;
 
   /// `avilaSnackBarTheme` is being used to handle the theme of
   /// [AvilaSnackBar] widget
-  final AvilaSnackBarTheme avilaSnackBarTheme;
+  final AvilaSnackBarTheme? avilaSnackBarTheme;
 
   /// `extensions` is used to get all AvilaTek theme extensions, to then
   /// define in the `MaterialApp` theme
   Iterable<ThemeExtension<dynamic>> get extensions => {
-        titleWrapperStyle,
-        selectorButtonStyle,
-        avilaSnackBarTheme,
+        titleWrapperStyle ?? const TitleWrapperStyle(),
+        selectorButtonStyle ?? const SelectorButtonStyle(),
+        avilaSnackBarTheme ?? const AvilaSnackBarTheme(),
       };
 }
