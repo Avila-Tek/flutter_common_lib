7aef634f01e2138a1f452b4ae84c570c011aad6d
Andres Eloy <andres.epacheco99@gmail.com>
2024-05-27 10:26:02 -0400

    feat: added http/https apiUrl assertion

--- packages/avilatek_core/lib/src/utils/assertions.dart
+++ packages/avilatek_core/lib/src/utils/assertions.dart
@@ -4,13 +4,18 @@ class CommonAssertions {
   ///
   /// Should be used when instancing a new GraphQL API object.
   static void assertGraphQLApi(String apiUrl) {
-    assert(
-      apiUrl.startsWith(RegExp('^https?://')),
-      'apiUrl format error: URL must start with "http://" or "https://" protocol',
-    );
+    assertUrlIsHttpOrHttps(apiUrl);
     assert(
       !apiUrl.endsWith('/') || !apiUrl.endsWith('graphql'),
       'apiUrl format error: URL cannot end with "/" nor "/graphql"',
+    );
+  }
+
+  /// Checks whether the apiUrl follows http or https protocol.
+  static void assertUrlIsHttpOrHttps(String apiUrl) {
+    assert(
+      apiUrl.startsWith(RegExp('^https?://')),
+      'apiUrl format error: URL must start with "http://" or "https://" protocol',
     );
   }
 }
