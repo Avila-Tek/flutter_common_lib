98e15be18346a1d559c170b5e2d87bef379a56cc
Andres Eloy <andres.epacheco99@gmail.com>
2024-07-02 13:40:37 -0400

    fix: fixed code warnings and removed example folder

--- examples/code_standards/lib/src/presentation/pokerandomizer/bloc/pokerandomizer_bloc.dart
+++ examples/code_standards/lib/src/presentation/pokerandomizer/bloc/pokerandomizer_bloc.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
 part 'pokerandomizer_event.dart';
 part 'pokerandomizer_state.dart';
 
-class PokerandomizerBloc extends Bloc<PokerandomizerEvent, PokerandomizerState> {
-  PokerandomizerBloc() : super(const PokerandomizerState()) {
-  }
+class PokerandomizerBloc
+    extends Bloc<PokerandomizerEvent, PokerandomizerState> {
+  PokerandomizerBloc() : super(const PokerandomizerState()) {}
 }
